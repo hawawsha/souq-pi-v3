b384150a63b4548b12f885a54546deba970ef8c7
AHMAD SULEIMAN MUSALLAM AlHawawsheh <38798371+hawawsha@users.noreply.github.com>
2026-07-04 10:31:58 +0300

    Add product details display in product page

--- pages/product/[productId].js
+++ pages/product/[productId].js
@@ -65,3 +65,71 @@ export default function ProductDetails() {
           gap: 40,
         }}
       >
+        <div>
+          {product.images?.length > 0 && (
+            <img
+              src={product.images[0]}
+              alt={product.name}
+              style={{
+                width: "100%",
+                borderRadius: 12,
+                objectFit: "cover",
+              }}
+            />
+          )}
+        </div>
+
+        <div>
+          <h1>{product.name}</h1>
+
+          <p
+            style={{
+              marginTop: 20,
+              lineHeight: 1.8,
+            }}
+          >
+            {product.description}
+          </p>
+
+          <h2
+            style={{
+              color: "#00b894",
+              marginTop: 25,
+            }}
+          >
+            {product.price} PI
+          </h2>
+
+          <p>
+            <b>Category:</b> {product.category}
+          </p>
+
+          <p>
+            <b>Stock:</b> {product.stock}
+          </p>
+
+          <p>
+            <b>Seller:</b>{" "}
+            {product.seller?.username || "Souq Pi"}
+          </p>
+
+          <button
+            style={{
+              marginTop: 30,
+              width: "100%",
+              padding: 16,
+              background: "#6c5ce7",
+              color: "#fff",
+              border: "none",
+              borderRadius: 8,
+              fontSize: 18,
+              cursor: "pointer",
+            }}
+          >
+            Buy Now
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
