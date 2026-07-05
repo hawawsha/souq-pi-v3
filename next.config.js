6c9df9b07ade5d6c0c6129d1a1cf534a3d82751d
AHMAD SULEIMAN MUSALLAM AlHawawsheh <38798371+hawawsha@users.noreply.github.com>
2026-07-05 17:47:56 +0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -12,10 +12,7 @@ const nextConfig = {
             key: "X-Content-Type-Options",
             value: "nosniff",
           },
-          {
-            key: "X-Frame-Options",
-            value: "DENY",
-          },
+          
           {
             key: "X-XSS-Protection",
             value: "1; mode=block",
