720024c0b6f973636888dd3bd26d63f5837a8a85
AHMAD SULEIMAN MUSALLAM AlHawawsheh <38798371+hawawsha@users.noreply.github.com>
2026-07-11 11:36:48 +0300

    Update route.js

--- app/api/orders/route.js
+++ app/api/orders/route.js
@@ -1,4 +1,6 @@
 // app/api/orders/route.js
+export const dynamic = "force-dynamic";
+
 import { NextResponse } from "next/server";
 import dbConnect from "@/lib/dbConnect";
 import Order from "@/models/Order";
